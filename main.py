0dd26f4498a017412e3e07c2f7f7b448cbb73421
Flowmikro <cloplop091@gmail.com>
2023-08-15 02:18:56 +0300

    tg_bot_API

--- main.py
+++ main.py
@@ -1,48 +1,65 @@
 import telebot, types
+import requests
+import json
 from telebot import types
 import sqlite3
 
 bot = telebot.TeleBot('5990529068:AAHSDaOozhfSVo5YrGHRqxAKBvI1OkXJX3Q')
-name = ''
+API = 'ba8721779b7dd2ff6a522ad14e069394'
 
 
 @bot.message_handler(commands=['start'])
-def message_start(message):
-    conn = sqlite3.connect('start.sql')  # старт БД, храниться вся БД
-    cur = conn.cursor()  # с помощью него можно управлять БД
-    cur.execute(
-        'CREATE TABLE IF NOT EXISTS user (id int auto_increment primary key, name varchar(50), password varchar(50))'
-    )
-    conn.commit()
-    cur.close()
-    conn.close()
-
-    bot.send_message(message.chat.id, 'Привет, дальше регистрация')
-    bot.register_next_step_handler(message, user_name)
+def start(message):
+    bot.send_message(message.chat.id, 'Укажите город:')
 
 
-def user_name(message):
-    global name
-    name = message.text.strip()
-    bot.send_message(message.chat.id, 'Теперь придумай пароль)))')
-    bot.register_next_step_handler(message, user_password)
+@bot.message_handler(content_types=['text'])
+def get_weather(message):
+    city = message.text.strip().lower()
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
+    data = json.loads(res.text)
+    bot.reply_to(message, f'Сейчас температура: {data["main"]["temp"]}')
 
 
-def user_password(message):
-    password = message.text.strip()
 
-    conn = sqlite3.connect('start.sql')  # старт БД, храниться вся БД
-    cur = conn.cursor()  # с помощью него можно управлять БД
-    cur.execute(
-        'INSERT INTO user (name, password) VALUES ("%s", "%s")' % (name, password)
-    )
-    conn.commit()
-    cur.close()
-    conn.close()
-
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardMarkup('Список пользователей'))
-    bot.send_message(message.chat.id, 'Вы зареганы')
+# name = ''
+# @bot.message_handler(commands=['start'])
+# def message_start(message):
+#     conn = sqlite3.connect('start.sql')  # старт БД, храниться вся БД
+#     cur = conn.cursor()  # с помощью него можно управлять БД
+#     cur.execute(
+#         'CREATE TABLE IF NOT EXISTS user (id int auto_increment primary key, name varchar(50), password varchar(50))'
+#     )
+#     conn.commit()
+#     cur.close()
+#     conn.close()
+#
+#     bot.send_message(message.chat.id, 'Привет, дальше регистрация')
+#     bot.register_next_step_handler(message, user_name)
+#
+#
+# def user_name(message):
+#     global name
+#     name = message.text.strip()
+#     bot.send_message(message.chat.id, 'Теперь придумай пароль)))')
+#     bot.register_next_step_handler(message, user_password)
+#
+#
+# def user_password(message):
+#     password = message.text.strip()
+#
+#     conn = sqlite3.connect('start.sql')  # старт БД, храниться вся БД
+#     cur = conn.cursor()  # с помощью него можно управлять БД
+#     cur.execute(
+#         'INSERT INTO user (name, password) VALUES ("%s", "%s")' % (name, password)
+#     )
+#     conn.commit()
+#     cur.close()
+#     conn.close()
+#
+#     markup = types.InlineKeyboardMarkup()
+#     markup.add(types.InlineKeyboardMarkup('Список пользователей '))
+#     bot.send_message(message.chat.id, 'Вы зареганы')
 
 
 @bot.message_handler(commands=['help'])
@@ -50,10 +67,10 @@ def message_help(message):
     bot.send_message(message.chat.id, 'Это бот')
 
 
-@bot.message_handler()
-def message_text(message):
-    bot.send_message(message.chat.id, 'Я ограниченный бот\n'
-                                      'инфо /help')
+# @bot.message_handler()
+# def message_text(message):
+#     bot.send_message(message.chat.id, 'Я ограниченный бот\n'
+#                                       'инфо /help')
 
 
 bot.polling(none_stop=True)
